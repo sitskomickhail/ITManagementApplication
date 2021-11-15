38a8445cbdeedc00ac3c939d72357c826938f4b3
Mikhail Sitsko <masitsko@gmail.com>
2021-11-15 21:27:13 +0300

    Updating view of Workers list

--- ITManagementClient/ViewModels/UserControls/WorkersListViewModel.cs
+++ ITManagementClient/ViewModels/UserControls/WorkersListViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 using System.Windows.Navigation;
@@ -13,21 +14,94 @@ namespace ITManagementClient.ViewModels.UserControls
     public class WorkersListViewModel : BaseViewModel, IControlViewModel
     {
         public IEnumerable<PageDefinition> PageDefinitions => new List<PageDefinition> { PageDefinition.Administrator };
-        
+
         public ObservableCollection<WorkerObservableModel> WorkersList { get; set; }
 
-
-        private void ShowWorkerDetailsCommandExecute(object obj)
+        private string _workerName;
+        public string WorkerName
         {
-
+            get => _workerName;
+            set { _workerName = value; OnPropertyChanged(nameof(WorkerName)); }
         }
 
+        public IEnumerable<string> RolesList { get; set; } = new List<string> { "Разработчик", "Ресурсный менеджер", "HR-менеджер", "Системный администратор" };
+
+        private string _selectedRole;
+        public string SelectedRole
+        {
+            get => _selectedRole;
+            set { _selectedRole = value; OnPropertyChanged(nameof(SelectedRole)); }
+        }
+
+        private string _workerSalary;
+        public string WorkerSalary
+        {
+            get => _workerSalary;
+            set
+            {
+                if (decimal.TryParse(value, out _))
+                {
+                    _workerSalary = value;
+                    OnPropertyChanged(nameof(WorkerSalary));
+                }
+            }
+        }
+
+        private DateTime _workerBirthDate;
+        public DateTime WorkerBirthDate
+        {
+            get => _workerBirthDate;
+            set { _workerBirthDate = value; OnPropertyChanged(nameof(WorkerBirthDate)); }
+        }
+
+        private DateTime _workerHireDate;
+        public DateTime WorkerHireDate
+        {
+            get => _workerHireDate;
+            set { _workerHireDate = value; OnPropertyChanged(nameof(WorkerHireDate)); }
+        }
+
+        private bool _workerAccountActive;
+        public bool WorkerAccountActive
+        {
+            get => _workerAccountActive;
+            set { _workerAccountActive = value; OnPropertyChanged(nameof(WorkerAccountActive)); }
+        }
+
+        private string _workerEnglishLevel;
+        public string WorkerEnglishLevel
+        {
+            get => _workerEnglishLevel;
+            set { _workerEnglishLevel = value; OnPropertyChanged(nameof(WorkerEnglishLevel)); }
+        }
+
+        private string _workerDepartmentName;
+        public string WorkerDepartmentName
+        {
+            get => _workerDepartmentName;
+            set { _workerDepartmentName = value; OnPropertyChanged(nameof(WorkerDepartmentName)); }
+        }
+
+        private string _login;
+        public string Login
+        {
+            get => _login;
+            set { _login = value; OnPropertyChanged(nameof(Login)); }
+        }
+
+        private string _searchParameter;
+        public string SearchParameter
+        {
+            get => _searchParameter;
+            set { _searchParameter = value; OnPropertyChanged(nameof(SearchParameter)); }
+        }
+
+        public ICommand SearchByParameterCommand { get; set; }
+        public ICommand SaveUpdatedWorkerCommand { get; set; }
 
         public WorkersListViewModel()
         {
             WorkersList = new ObservableCollection<WorkerObservableModel>();
-
-            //ShowWorkerDetailsCommand = new RelayCommand(ShowWorkerDetailsCommandExecute);
 
             WorkersList.Add(new WorkerObservableModel()
             {
@@ -50,6 +124,24 @@ namespace ITManagementClient.ViewModels.UserControls
                 WorkerId = 10,
                 ShowWorkerDetailsCommand = new RelayCommand(ShowWorkerDetailsCommandExecute)
             });
+
+            SaveUpdatedWorkerCommand = new RelayCommand(SaveUpdatedWorkerCommandExecute);
+            SearchByParameterCommand = new RelayCommand(SearchByParameterCommandExecute);
+        }
+
+        private void ShowWorkerDetailsCommandExecute(object obj)
+        {
+
+        }
+
+        private void SaveUpdatedWorkerCommandExecute(object obj)
+        {
+
+        }
+
+        private void SearchByParameterCommandExecute(object obj)
+        {
+
         }
     }
 }
